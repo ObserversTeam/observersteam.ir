a9fb656e89f26a63642d69708ce904c689cd40ad
Ho3einWave <Ho3ein8280@gmail.com>
2022-05-16 23:16:31 +0430

    scroll with js

--- js/app.js
+++ js/app.js
@@ -3,14 +3,19 @@ let NavVisibily = false;
 $(".menu").click(function () {
   if (!NavVisibily) {
     $(".menu-items").attr("data-menu-state", "open");
+    $("body").css("overflow", "hidden");
     NavVisibily = true;
   } else if (NavVisibily) {
     $(".menu-items").attr("data-menu-state", "close");
+    $("body").css("overflow", "scroll");
+
     NavVisibily = false;
   }
 });
 
 $(".menu-item").click(function () {
   $(".menu-items").attr("data-menu-state", "close");
+  $("body").css("overflow", "scroll");
+
   NavVisibily = false;
 });
