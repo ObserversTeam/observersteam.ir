143d7c2509e0c741950a963bf00bc2fd873f386a
Ho3einWave <Ho3ein8280@gmail.com>
2022-05-16 23:55:06 +0430

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ let NavVisibily = false;
 $(".menu").click(function () {
   if (!NavVisibily) {
     $(".menu-items").attr("data-menu-state", "open");
-    $("body").css("overflow", "hidden");
+    $("body").css("overflow-y", "hidden");
     NavVisibily = true;
   } else if (NavVisibily) {
     $(".menu-items").attr("data-menu-state", "close");
-    $("body").css("overflow", "scroll");
+    $("body").css("overflow-y", "scroll");
 
     NavVisibily = false;
   }
@@ -15,7 +15,7 @@ $(".menu").click(function () {
 
 $(".menu-item").click(function () {
   $(".menu-items").attr("data-menu-state", "close");
-  $("body").css("overflow", "scroll");
+  $("body").css("overflow-y", "scroll");
 
   NavVisibily = false;
 });
